c2a0476deac3e67f577a6806b86ada781d2cbccd
ThanhDo <thanhdo121998@gmail.com>
2019-10-01 15:09:02 +0700

    update code

--- view/edit.php
+++ view/edit.php
@@ -6,17 +6,19 @@ $line2 = LearningHistory::getListFromFile("101");
     if(isset($_GET["edit"])&&isset($_POST["submit2"])){ 
         $id=$_GET["edit"];
         $lines2 = file("../resource/learninghistory.txt",FILE_IGNORE_NEW_LINES);
-        $fo = fopen("../resource/filephu.txt","w");    
+        $fo = fopen("../resource/filephu.txt","w");  
+        $num5 = $_POST["txt5"];
+        $num6 = $_POST["txt6"];
+        $num7 = $_POST["txt7"];
+        $num8 = $_POST["txt8"];  
      foreach ($lines2 as $key => $value) {
             $arr2 = explode("#",$value);
             if($arr2[0]!=$id)
                 fwrite($fo,$arr2[0]."#".$arr2[1]."#".$arr2[2]."#".$arr2[3]."#".$arr2[4]."#101\n");  
+            else{
+                fwrite($fo,$id."#".$num5."#".$num6."#".$num7."#".$num8."#101\n");
+            }
         }
-        $num5 = $_POST["txt5"];
-        $num6 = $_POST["txt6"];
-        $num7 = $_POST["txt7"];
-        $num8 = $_POST["txt8"];
-        fwrite($fo,$id."#".$num5."#".$num6."#".$num7."#".$num8."#101\n");
         fclose($fo);
         $lines1 = file("../resource/filephu.txt",FILE_IGNORE_NEW_LINES);
         $fo2 = fopen("../resource/learninghistory.txt","w"); 
